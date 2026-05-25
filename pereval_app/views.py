8b6781ea3e1045e292955909e112f72a7eb3491a
Name <email@example.ru>
2026-05-25 15:54:27 +0300

    sprint2 get objects with email

--- pereval_app/views.py
+++ pereval_app/views.py
@@ -42,3 +42,30 @@ class SubmitDataView(APIView):
                 },
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+
+    def get(self, request):
+        email = request.query_params.get('user__email')
+
+        if not email:
+            return Response(
+                {"message": "Не указан email пользователя", "status": 400},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            pereval_list = Pereval.objects.filter(user__email=email)
+
+            if not pereval_list.exists():
+                return Response(
+                    {"message": "Нет записей для указанного пользователя", "status": 404},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+            serializer = PerevalSerializer(pereval_list, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response(
+                {"message": f"Ошибка сервера: {str(e)}", "status": 500},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
