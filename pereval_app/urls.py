7ccfe6da4e38b0da6b342c4d8b043877bc9d8494
Name <email@example.ru>
2026-05-26 15:26:29 +0300

    common edit

--- pereval_app/urls.py
+++ pereval_app/urls.py
@@ -9,7 +9,7 @@ router.register(r'pereval_list', PerevalViewSet, basename='pereval_list')
 
 urlpatterns = [
     path("submitData/", SubmitDataView.as_view(), name="submit_data"),
-    path("pereval_list/<int:id>/", SubmitDataDetailView.as_view(), name="submit_data_detail"),
+    path("submitData/<int:id>/", SubmitDataDetailView.as_view(), name="submit_data_detail"),
 ]
 
 urlpatterns += router.urls
